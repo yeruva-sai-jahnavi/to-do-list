75d5b3ae245574502cd513d2432958332643e98a
Yeruva Sai Jahnavi <saijahnavi.yeruva@gmail.com>
2023-06-11 17:33:41 +0530

    create task model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE, null = True, blank = True) # manage everything to do with app
+    title = models.CharField(max_length=200, null = True)
+    description = models.TextField(null = True, blank = True)
+    complete = models.BooleanField(default = False)
+    created = models.DateTimeField(auto_now_add=True) # auto make current time the created time
