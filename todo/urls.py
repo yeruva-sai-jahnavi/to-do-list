a074d0ba72e7b00521b6ad9bb6a1b9a4bbaf7b26
Yeruva Sai Jahnavi <saijahnavi.yeruva@gmail.com>
2023-06-17 12:35:38 +0530

    Create link to each individual task

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,5 @@ from .views import TaskListView, TaskDetailView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name="task"),
-    path('task/<int:pk>/', TaskDetailView.as_view(), name="task_detail"),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name="tasks_detail"),
 ]
