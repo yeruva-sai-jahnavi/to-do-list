851580b5f3613522149cc5ebc6ee33a2c7157c96
Yeruva Sai Jahnavi <saijahnavi.yeruva@gmail.com>
2023-06-18 12:55:14 +0530

    Create add task view and mapped to url

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,5 @@ from .views import TaskListView, TaskDetailView, TaskCreate
 urlpatterns = [
     path('', TaskListView.as_view(), name="task"),
     path('task-create/', TaskCreate.as_view(), name="task-create"),
-    path('task/<int:pk>/', TaskDetailView.as_view(), name="tasks-detail"),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name="task-detail"),
 ]
